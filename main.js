d1a0b40b9f3a5e17cee3bdfdaad01408aaac9b5a
Timothy Do <timothydobsa@gmail.com>
2021-01-13 02:07:44 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var workerCounter = 0;
-var memBackground1, memBackground2, memBackground3, memBackground4;
+var memBackground;
 function countWorker() {
 	var counter = document.getElementById("counter");
 	counter.innerHTML = "<h2 style=\"font-family:Courier; font-size: 1.5em\">Webworkers Active: " + workerCounter;
@@ -8,7 +8,7 @@ function countWorker() {
 function memTest() {
 	proceed = confirm('Are You Ok With Background Processes & Sudden Crashes?')
 	if(proceed == true && window.Worker) {
-		memBackground1 = new Worker('mem.js');
+		memBackground = new Worker('mem.js');
 		workerCounter += 1;
 		countWorker();
 	}
