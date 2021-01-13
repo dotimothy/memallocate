803c8e2936c7fdb0dd189eb8f07eac5f90125e1b
Timothy Do <timothydobsa@gmail.com>
2021-01-12 19:31:12 -0800

    4 Times the Fun

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var workerCounter = 0;
-var memBackground;
+var memBackground1, memBackground2, memBackground3, memBackground4;
 function countWorker() {
 	var counter = document.getElementById("counter");
 	counter.innerHTML = "<h2 style=\"font-family:Courier; font-size: 1.5em\">Webworkers Active: " + workerCounter;
@@ -8,8 +8,11 @@ function countWorker() {
 function memTest() {
 	proceed = confirm('Are You Ok With Background Processes & Sudden Crashes?')
 	if(proceed == true) {
-		memBackground = new Worker('mem.js');
+		memBackground1 = new Worker('mem.js');
+		memBackground2 = new Worker('mem.js');
+		memBackground3 = new Worker('mem.js');
+		memBackground4 = new Worker('mem.js');
 	}
-	workerCounter++;
+	workerCounter += 4;
 	countWorker();
 }
