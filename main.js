4f598b966d28edfd7d205c04de5472e63013e1b6
Timothy Do <timothydobsa@gmail.com>
2021-01-12 18:53:59 -0800

    ok

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+var workerCounter = 0;
+function countWorker() {
+	var counter = document.getElementById("counter");
+	counter.innerHTML = "<h2 style=\"font-family:Courier; font-size: 1.5em\">Webworkers Active: " + workerCounter;
+
+}
 function memTest() {
 	proceed = confirm('Are You Ok With Background Processes & Sudden Crashes?')
 	if(proceed == true) {
 		var memBackground = new Worker('mem.js');
 	}
+	workerCounter++;
+	countWorker();
 }
