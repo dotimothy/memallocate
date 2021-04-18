2667f0faf0665e6ba2413ee2b25c9fb044357927
Timothy Do <timothydobsa@gmail.com>
2021-04-18 01:05:30 -0700

    added disclaimer

--- memocate.py
+++ memocate.py
@@ -1,6 +1,9 @@
-#Memocate
+#memocate.py: The Trivial Memory Allocator in Python
+#Disclaimer: Use this at your own discretion. I am not responsible for anything that happens.
+#Author: TD
 
+#initialization
 a = "a"
-b = "b"
+#Storing string to memory until overflow.
 while(1):
 	a = a + a
